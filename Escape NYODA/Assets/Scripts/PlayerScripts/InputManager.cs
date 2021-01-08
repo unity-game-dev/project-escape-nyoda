ca41ded19808ccb55ab63aff15f46f0afebe244c
Debashish Majumdar <majumdardebashish@ymail.com>
2021-01-09 01:48:12 +0530

    added player movements and sprites

---
+++ Escape NYODA/Assets/Scripts/PlayerScripts/InputManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace projectNyodaToolkit
+{
+    public class InputManager : MonoBehaviour
+    {
+        [SerializeField] protected KeyCode croucH;
+        [SerializeField] protected KeyCode sprintinG;
+        [SerializeField] protected KeyCode jumP;
+        // Update is called once per frame
+        void Update()
+        {
+            CrouchHeld();
+            SprintingHeld();
+            JumpPressed();
+        }
+        public virtual bool CrouchHeld()
+        {
+            if (Input.GetKey(croucH))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public virtual bool SprintingHeld()
+        {
+            if (Input.GetKey(sprintinG))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public virtual bool JumpPressed()
+        {
+            if (Input.GetKeyDown(jumP))
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+    }
+}
+
