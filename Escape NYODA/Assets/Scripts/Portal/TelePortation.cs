75b452135809288139775b70610137cc45394756
Debashish Majumdar <majumdardebashish@ymail.com>
2021-01-23 05:13:08 +0530

    minor fixes

--- Escape NYODA/Assets/Scripts/Portal/TelePortation.cs
+++ Escape NYODA/Assets/Scripts/Portal/TelePortation.cs
@@ -6,14 +6,24 @@ public class TelePortation : MonoBehaviour
 {
     public GameObject Portal, Player;
     public static int StartTeleport = 0;
-    
+    public bool canTeleport;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E) && (canTeleport))
+        {
+            StartCoroutine(Teleport());
+            canTeleport = false;
+        }
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Player" && TelePortation.StartTeleport == 1)
+        if (collision.gameObject.tag == "Player" && TelePortation.StartTeleport == 1)
         {
+            canTeleport = true;
             TelePortation.StartTeleport = 0;
-            StartCoroutine(Teleport());
         }
+
     }
 
     IEnumerator Teleport()
