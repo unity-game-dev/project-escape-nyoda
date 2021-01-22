8542954eee834e521f41d506b5ea2852acbc3de4
Parth Bhardwaj <53986600+RuntimeTerror10@users.noreply.github.com>
2021-01-23 02:49:42 +0530

    added jump and walking sound scripts

---
+++ Escape NYODA/Assets/Scripts/IgorScripts/walkSound.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class walkSound : MonoBehaviour
+{
+      public AudioSource someSound; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A)||Input.GetKeyDown(KeyCode.D)){
+            someSound.Play();
+            someSound.loop = true;
+        }
+        else if (Input.GetKeyUp (KeyCode.A)||Input.GetKeyUp (KeyCode.D)) {
+         someSound.Stop ();
+     }
+    }
+}
