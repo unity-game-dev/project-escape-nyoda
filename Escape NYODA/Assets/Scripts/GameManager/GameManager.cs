47b3701ef354454737ef81d0b59ea7fae365f00b
Debashish Majumdar <majumdardebashish@ymail.com>
2021-01-24 04:06:26 +0530

    added game manager script

---
+++ Escape NYODA/Assets/Scripts/GameManager/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameHasEnded = false;
+    public float restartWaitTime = 2f;
+    public void EndGame()
+    {
+        if(gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("GAME OVER");
+            Invoke("Restart", restartWaitTime);
+        }
+        
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
