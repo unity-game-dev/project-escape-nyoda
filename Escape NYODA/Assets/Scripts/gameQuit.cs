6d416f765e3970abdb1e31dff93fa8b04e260dfb
Parth Bhardwaj <53986600+RuntimeTerror10@users.noreply.github.com>
2021-01-24 03:34:40 +0530

    main menu with sounds and anim added

---
+++ Escape NYODA/Assets/Scripts/gameQuit.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameQuit : MonoBehaviour
+{
+   public void quit() {
+       Debug.Log("has quit game");
+		Application.Quit();
+	}
+}
