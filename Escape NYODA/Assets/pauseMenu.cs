11957e86598c3c0b3ab4b6bb4fe8bb9f2ae145c3
Parth Bhardwaj <53986600+RuntimeTerror10@users.noreply.github.com>
2021-01-24 23:39:09 +0530

    kachra

--- Escape NYODA/Assets/pauseMenu.cs
+++ Escape NYODA/Assets/pauseMenu.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class pauseMenu : MonoBehaviour
 {
    public static bool GameIsPaused = false;
+
    public GameObject pauseMenuUI;
     public GameObject playerUI;
     void Update()
@@ -19,19 +21,34 @@ public class pauseMenu : MonoBehaviour
         }
     }
     public void Resume(){
+   
         playerUI.SetActive(true);
         pauseMenuUI.SetActive(false);
         Time.timeScale=1f;
         GameIsPaused=false;
+        
+        if((SceneManager.GetActiveScene().name=="PlayerScene")){
+            Debug.Log((SceneManager.GetActiveScene().name=="PlayerScene"));
+                AudioManager.instance.Play("BgMusic");
+
+        }
+        else{
+                     AudioManager.instance.StopPlaying("BgMusic");
+        }
+      
     }
     void Pause(){
         playerUI.SetActive(false);
         pauseMenuUI.SetActive(true);
         Time.timeScale=0f;
         GameIsPaused = true;
+
     }
     public void QuitGame(){
-        Debug.Log("quitting game");
-        Application.Quit();
+        Debug.Log("going to menu");
+        SceneManager.LoadScene("MainMenu");
+        GameIsPaused=false;
+        Resume();
+  
     }
 }
