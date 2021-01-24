f22349ae4a5b4d771964a26ca552b7267c880b02
Parth Bhardwaj <53986600+RuntimeTerror10@users.noreply.github.com>
2021-01-24 17:23:47 +0530

    added pause menu

---
+++ Escape NYODA/Assets/pauseMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class pauseMenu : MonoBehaviour
+{
+   public static bool GameIsPaused = false;
+   public GameObject pauseMenuUI;
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            if(GameIsPaused){
+                Resume();
+            }
+            else{
+                Pause();
+            }
+        }
+    }
+    public void Resume(){
+        pauseMenuUI.SetActive(false);
+        Time.timeScale=1f;
+        GameIsPaused=false;
+    }
+    void Pause(){
+        pauseMenuUI.SetActive(true);
+        Time.timeScale=0f;
+        GameIsPaused = true;
+    }
+    public void QuitGame(){
+        Debug.Log("quitting game");
+        Application.Quit();
+    }
+}
