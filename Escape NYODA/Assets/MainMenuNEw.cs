8858207a09b42b89d1645f2724d41b79c06b357c
Rohit Singh <55148217+Rohit14kvs@users.noreply.github.com>
2021-01-24 14:04:13 +0530

    Scene Transsition

---
+++ Escape NYODA/Assets/MainMenuNEw.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenuNEw : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void PlayGame(string level)
+    {
+        // SceneManager.LoadScene(""); //Scene Name eg: level 1
+        SceneManager.LoadScene(level); //for continuation
+        //Menu Index = 0, Game Index = 1
+
+    }
+}
