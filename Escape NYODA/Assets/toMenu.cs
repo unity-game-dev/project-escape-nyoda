c6144745f357eaa4f7f13f960a28ea9d6b520ab8
Parth Bhardwaj <53986600+RuntimeTerror10@users.noreply.github.com>
2021-01-25 04:27:37 +0530

    Thank you scene added

---
+++ Escape NYODA/Assets/toMenu.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+ 
+public class toMenu : MonoBehaviour
+{
+    public void NextScene(){
+    
+        SceneManager.LoadScene("MainMenu");
+    }
+}
