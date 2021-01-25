9c5d2510ca38a0e8d69c2aa8bfc411d254ea8397
Parth Bhardwaj <53986600+RuntimeTerror10@users.noreply.github.com>
2021-01-26 02:05:21 +0530

    instructions overlay completed

---
+++ Escape NYODA/Assets/barrierToPotions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class barrierToPotions : MonoBehaviour
+{
+    public GameObject potionUI;
+    public GameObject barrierUI;
+    public void openPotion(){
+        potionUI.SetActive(true);
+        barrierUI.SetActive(false);
+    }
+}
---
+++ Escape NYODA/Assets/potionToMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class potionToMenu : MonoBehaviour
+{
+    public GameObject MenuUI;
+    public GameObject PotionUI;
+    public void open(){
+        MenuUI.SetActive(true);
+        PotionUI.SetActive(false);
+    }
+}
