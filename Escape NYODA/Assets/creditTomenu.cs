5ea3de5608186720de7095db1c8be9583cb2905f
Parth Bhardwaj <53986600+RuntimeTerror10@users.noreply.github.com>
2021-01-25 18:55:25 +0530

    added credits overlay

---
+++ Escape NYODA/Assets/creditTomenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class creditTomenu: MonoBehaviour
+{
+    public GameObject controlsUI;
+    public GameObject MainMenuUI;
+    public void creditclose(){
+        controlsUI.SetActive(false);
+        MainMenuUI.SetActive(true);
+    }
+}
---
+++ Escape NYODA/Assets/menutocredit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menutocredit : MonoBehaviour
+{
+    public GameObject controlsUI;
+    public GameObject MainMenuUI;
+    public void creditOpen(){
+        controlsUI.SetActive(true);
+        MainMenuUI.SetActive(false);
+    }
+}
