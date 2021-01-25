4eb4127b90940a727c7e2a1ddc32936e25f58c3a
Parth Bhardwaj <53986600+RuntimeTerror10@users.noreply.github.com>
2021-01-26 01:35:20 +0530

    added gemScript

---
+++ Escape NYODA/Assets/goToGems.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class goToGems : MonoBehaviour
+{
+    public GameObject GemUI;
+    public GameObject controlUI;
+    public void openGems(){
+        GemUI.SetActive(true);
+        controlUI.SetActive(false);
+    }
+}
