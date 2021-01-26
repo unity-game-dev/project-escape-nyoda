ba10dc118c7ac6639ba6ca0e324233a6dcb6fd77
Parth Bhardwaj <53986600+RuntimeTerror10@users.noreply.github.com>
2021-01-26 14:04:23 +0530

    added loading screen

---
+++ Escape NYODA/Assets/loadingtoMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class loadingtoMenu : MonoBehaviour
+{   //public string levelToLoad;
+    //rivate float timer = 8f;
+    //private Text timerSeconds;
+
+    void Start () {
+
+    StartCoroutine (loadSceneAfterDelay(8));
+
+    }
+
+    IEnumerator loadSceneAfterDelay(float waitbySecs){
+
+        yield return new WaitForSeconds(waitbySecs);
+        Application.LoadLevel (1);
+    } 
+}
+    
